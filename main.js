1f129a332665aa3988fdd9bd90fc17865866d0be
Leo Veres <leoveres@gmail.com>
2018-05-07 15:02:34 -0400

    5th push

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 
-
+function yearOfBirth(age){
+    return 2016-age;
+}
 
 function whoAmI(name, age){
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
 
-  var yearOfBirth = 2016 - age;
+  var yob = yearOfBirth(age);
 
-  console.log(`I was born in ${yearOfBirth}` )
+  console.log(`I was born in ${yob}`);
 }
 
 whoAmI();
