4507a5389b069e8fb89deb6db2a538ca093ca369
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-07 16:31:57 -0400

    cracking

--- drills2.js
+++ drills2.js
@@ -1,4 +1,4 @@
-/*function jediname(firstName,lastName){
+function jediname(firstName,lastName){
     let jedifirst = firstName.slice(0, 2);
 
     let jedilast = lastName.slice(0, 3);
@@ -26,19 +26,37 @@ function beyond (num){
     else (num === 0){
         console.log('Staying home');
     }
-}*/
+}
 
 function decode(words){
   let dwords = words.split(" ");
-  let lastwords = " ";
 
-  for (i = 0; i < dwords.length; i++;){
-    if (dwords[0] = "a"){
-      dwords[1] = lastwords;
+
+  for (i = 0; i < dwords.length; i++){
+   let lastwords = dwords[i];
+    if (lastwords[0] === "a" ){
+
+      console.log(lastwords[1]);
     }
+    else if (lastwords[0] === "b" ){
+
+      console.log(lastwords[2]);
+    }
+    else if (lastwords[0] === "c" ){
+
+      console.log(lastwords[3]);
+    }
+    else if (lastwords[0] === "d" ){
+
+      console.log(lastwords[4]);
+    }
+    else {
+      console.log(" ");
+    }
+
 
   }
 
 }
 
-decode("craft block");
+decode("craft block argon meter bells brown croon droop");
