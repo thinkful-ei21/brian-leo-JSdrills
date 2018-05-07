c09d11341024ee577a93cbf1407159c4309dc2d6
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-07 16:05:42 -0400

    last pro

--- drills2.js
+++ drills2.js
@@ -1,4 +1,4 @@
-function jediname(firstName,lastName){
+/*function jediname(firstName,lastName){
     let jedifirst = firstName.slice(0, 2);
 
     let jedilast = lastName.slice(0, 3);
@@ -6,7 +6,7 @@ function jediname(firstName,lastName){
     return jedilast + jedifirst;
 }
 
-console.log(jediname("breian","dixon"));
+console.log(jediname("breian","dixon"));//
 
 
 
@@ -26,4 +26,19 @@ function beyond (num){
     else (num === 0){
         console.log('Staying home');
     }
+}*/
+
+function decode(words){
+  let dwords = words.split(" ");
+  let lastwords = " ";
+
+  for (i = 0; i < dwords.length; i++;){
+    if (dwords[0] = "a"){
+      dwords[1] = lastwords;
+    }
+
+  }
+
 }
+
+decode("craft block");
