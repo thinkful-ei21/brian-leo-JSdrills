5b0e3dd3dbd1f548a03c066246bc1231ca53519a
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-07 14:58:59 -0400

    forth push

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 
 function whoAmI(name, age){
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+
+  var yearOfBirth = 2016 - age;
+
+  console.log(`I was born in ${yearOfBirth}` )
 }
 
 whoAmI();
